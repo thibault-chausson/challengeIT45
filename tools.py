046fe0d7c21f40f57ec9216390f092f9f1ef5039
thibault-chausson <chausson.thibault@laposte.net>
2022-06-02 00:42:05 +0200

    tri en fonction du langage

--- tools.py
+++ tools.py
@@ -1,5 +1,57 @@
+import json as js
+import statistics as st
 import numpy as np
 import random as rd
+
+MATRICE_DISTANCE = []
+INTERVENANTS = []
+MISSIONS = []
+
+SOLUTIONS = []  # Les lignes representent les formateurs (interfaces) et les colones les missions
+
+
+def charge_fichier_csv(dossier):
+    """
+    Charge le contenue du fichier csv dans les variables globales
+    """
+    with open(f"Instances/{dossier}/Distances.csv", 'r') as fichier:
+        lignes = fichier.read().split('\n')
+        if lignes[-1] == '':
+            lignes = lignes[:-1]
+        for ligne in lignes:
+            MATRICE_DISTANCE.append(list(map(float, ligne.split(','))))
+
+    with open(f"Instances/{dossier}/Intervenants.csv", 'r') as fichier:
+        lignes = fichier.read().split('\n')
+        if lignes[-1] == '':
+            lignes = lignes[:-1]
+        for ligne in lignes:
+            INTERVENANTS.append(ligne.split(','))
+
+    with open(f"Instances/{dossier}/Missions.csv", 'r') as fichier:
+        lignes = fichier.read().split('\n')
+        if lignes[-1] == '':
+            lignes = lignes[:-1]
+        for ligne in lignes:
+            MISSIONS.append(ligne.split(','))
+
+    # Changements des chiffres de types str en type int
+    for i in range(len(INTERVENANTS)):
+        for j in range(len(INTERVENANTS[i])):
+            try:
+                INTERVENANTS[i][j] = int(INTERVENANTS[i][j])
+            except:
+                pass
+
+    for i in range(len(MISSIONS)):
+        for j in range(len(MISSIONS[i])):
+            try:
+                MISSIONS[i][j] = int(MISSIONS[i][j])
+            except:
+                pass
+
+    return (MATRICE_DISTANCE, INTERVENANTS, MISSIONS)
+
 
 exempleFitness = [[12, 98, 56, 9, 152, 64, 567, 74, 989],
                   [12, 98, 56, 9, 152, 64, 567, 74, 989],
@@ -42,4 +94,22 @@ def reproduction (solution1, solution2, ligDebut, ligFin, colDebut, colFin): #fo
             fille[i][j] = solution2[i][j]
     return fille
 
-print(reproduction(exempleFitness, exempleFitness2, 1,2,3,6 ))
+def tri_langage(Mission):
+    LPC = []
+    LSF= []
+    for i in range(len(Mission)):
+        if Mission[i][4] == "LPC":
+            LPC.append(Mission[i])
+        else:
+            LSF.append(Mission[i])
+    return LPC, LSF
+
+
+
+def main():
+    charge_fichier_csv("45-4")
+    print(tri_langage(MISSIONS))
+
+
+if __name__ == "__main__":
+    main()
