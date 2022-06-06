be61ce5d478a2416eecd4f2dbc5c3f962cb42799
thibault-chausson <chausson.thibault@laposte.net>
2022-06-06 13:39:53 +0200

    pareto test

--- test.py
+++ test.py
@@ -49,6 +49,43 @@ def pareto_frontier(Xs, Ys, Zs, minX = True, minY = True, minZ = True):
     return p_frontX, p_frontY, p_frontZ, solution_correspondante
 
 
+import numpy as np
+
+def pareto_frontier_multi(X,Y,Z):
+    myList = [[X[i], Y[i], Z[i]] for i in range(len(X))]
+    myArray = np.zeros((len(X), 3))
+    for i in range(len(X)):
+        myArray[i][0] = X[i]
+        myArray[i][1] = Y[i]
+        myArray[i][2] = Z[i]
+    # Sort on first dimension
+    myArray = myArray[myArray[:,0].argsort()]
+    # Add first row to pareto_frontier
+    pareto_frontier = myArray[0:1,:]
+    # Test next row against the last row in pareto_frontier
+    for row in myArray[1:,:]:
+        if sum([row[x] >= pareto_frontier[-1][x]
+                for x in range(len(row))]) != len(row):
+            # If it is better on all features add the row to pareto_frontier
+            pareto_frontier = np.concatenate((pareto_frontier, [row]))
+
+    pX = []
+    pY = []
+    pZ = []
+
+    for i in range(len(pareto_frontier)):
+        pX.append(pareto_frontier[i][0])
+        pY.append(pareto_frontier[i][1])
+        pZ.append(pareto_frontier[i][2])
+
+    par = [[pX[i], pY[i], pZ[i]] for i in range(len(pX))]
+
+    solution_correspondante = []
+
+    for i in par:
+        solution_correspondante.append(myList.index(i))
+
+    return pX, pY, pZ, solution_correspondante
 
 
 
@@ -63,13 +100,17 @@ Zs = [77.77777778, 73.33333333, 84.44444444, 71.11111111, 73.33333333, 64.444444
 
 
 # Find lowest values for cost and highest for savings
-p_front = pareto_frontier(Xs, Ys, Zs, minX= False, minY= True, minZ= True)
+p_front = pareto_frontier_multi(Xs, Ys, Zs)
 # Plot a scatter graph of all results
 fig = plt.figure()
 ax = fig.gca(projection='3d')  # Affichage en 3D
 ax.scatter(Xs, Ys, Zs, label='Courbe', marker='d')  # Tracé des points 3D
 # Then plot the Pareto frontier on top
 plt.plot(p_front[0], p_front[1],p_front[2], color='red')
+ax.set_xlabel('Objectif 1')
+ax.set_ylabel('Objectif 2')
+ax.set_zlabel('Objectif 3')
+ax.text2D(0.05, 0.95, "Front de Pareto", transform=ax.transAxes)
 plt.show()
 
 print(p_front)
